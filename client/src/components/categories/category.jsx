0c83f096513ceb1034e48f6d4a549a6237092702
SaidMayan <Kasemayan@gmail.com>
2018-08-11 16:08:23 -0400

    update heroku with latest changes

--- client/src/components/categories/category.jsx
+++ client/src/components/categories/category.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './category.css';
 
 function Category(props) {
 
   return(
     <Link to={`/categories/${props.category.toLocaleLowerCase()}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-    <div>
-        <h3>{props.category}</h3>
+    <div className="category-list">
+        <h2>{props.category}</h2><br />
     </div>
     </Link>
     )
